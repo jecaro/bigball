7db5b4c5a13f3dc85a2d96734be36c3fe7f95378
jecaro <jeancharles.quillet@gmail.com>
2025-06-06 12:36:51 +0200

    Bump to nixos-25.05

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   # inspired by: https://serokell.io/blog/practical-nix-flakes#packaging-existing-applications
   # description = "A Hello World in Haskell with a dependency and a devShell";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" ];
